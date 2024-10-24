d6c3d75c2b2744733616912c6617ef184e030e74
Greg Bernstein <gsbernst@gmail.com>
2024-10-23 19:40:55 -0700

    Initial Commit

---
+++ SpookyVision/SpookyVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpookyVisionApp.swift
+//  SpookyVision
+//
+//  Created by Greg Bernstein on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SpookyVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
